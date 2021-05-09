45ea708f07d894807d013296613e80bb0d3223c3
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-09 11:45:25 +0530

    Append method was created

--- Singly/src/ds/Node.java
+++ Singly/src/ds/Node.java
@@ -9,6 +9,22 @@ public class Node {
     }
 }
   class LinkedList{
+    Node head;
+
+    public  void  append(int data){
+        if(head==null){
+            head=new Node(data);
+            return;
+        }
+        Node current=head;
+        while(current.next != null){
+            current=current.next;
+        }
+        current.next=new Node(data);
+    }
+
+
+
 
 
 }
