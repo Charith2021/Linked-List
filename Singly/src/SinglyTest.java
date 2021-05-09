e9374933fdb55170daa35f84a7182dd920150e73
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-09 10:43:50 +0530

    The obeject of node was created

--- Singly/src/SinglyTest.java
+++ Singly/src/SinglyTest.java
@@ -1,2 +1,10 @@
+import ds.Node;
+
 public class SinglyTest {
+    public static void main(String[] args) {
+        Node node=new Node();
+        //node.prepend();
+       // node.append();
+        //node.deleteWithValue();
+    }
 }
