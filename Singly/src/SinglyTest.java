36d4bd4b91ddd1032ba79a278e98323a3092316f
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-09 10:49:45 +0530

    Parameterized constructor was added

--- Singly/src/SinglyTest.java
+++ Singly/src/SinglyTest.java
@@ -2,9 +2,9 @@ import ds.Node;
 
 public class SinglyTest {
     public static void main(String[] args) {
-        Node node=new Node();
-        //node.prepend();
-       // node.append();
+        Node node=new Node(10);
+       // node.apppend();
+       // node.prepend();
         //node.deleteWithValue();
     }
 }
--- Singly/src/ds/Node.java
+++ Singly/src/ds/Node.java
@@ -1,5 +1,10 @@
 package ds;
 
 public class Node {
+    Node next;
+    int data;
 
+    public Node(int data) {
+        this.data = data;
+    }
 }
